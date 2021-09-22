e3277fedf2508bbe953d3c6095382ec3c5f2aad4
Arturo León <arturo.leon@bahia360.mx>
2021-09-22 16:57:18 -0500

    QA inversion empresarial

--- src/Components/Consulta.jsx
+++ src/Components/Consulta.jsx
@@ -5,8 +5,8 @@ import styled from 'styled-components'
 const StyledConsulta = styled.div`
 margin-top: 50px;
 .text{
-    font-family: Futura;
-    font-size: 14px;
+    font-family: futura_light;
+    font-size: 16px;
     line-height: 110%;
     color: #636363;
 }
@@ -40,14 +40,16 @@ export default function Consulta() {
                 <div className="text">
                     <p>
                     UNE <br />
-                    BanCoppel S.A. Institución de Banca Múltiple recibe las consultas, reclamaciones o aclaraciones, en su Unidad Especializada de Atención a Usuarios, ubicada en Insurgentes Sur No. 553 Piso 6, Col. Escandón II, Alcaldía Miguel Hidalgo, C.P. 11800, Ciudad de México y por correo electrónico une@bancoppel.com o al teléfono (55) 5278 0000, así como en cualquiera de sus sucursales u oficinas.
+                    BanCoppel S.A. Institución de Banca Múltiple recibe las consultas, reclamaciones o aclaraciones, en su Unidad Especializada de Atención a Usuarios, ubicada en Insurgentes Sur No. 553 Piso 6, Col. Escandón II, Alcaldía Miguel Hidalgo, C.P. 11800, Ciudad de México y por correo electrónico <a href="mailto:une@bancoppel.com">une@bancoppel.com</a>
+ o al teléfono <a href="tel:5552780000">(55) 5278 0000</a>, así como en cualquiera de sus sucursales u oficinas.
                     </p>
                 </div>
 
                 <div className="text down">
                     <p>
                     CONDUSEF <br />
-                    En el caso de no obtener una respuesta satisfactoria, podrás acudir a la Comisión Nacional para la Protección y Defensa de los Usuarios de Servicios Financieros en www.condusef.gob.mx , o a los teléfonos (55) 5340 0999 y 800 999 8080.
+                    En el caso de no obtener una respuesta satisfactoria, podrás acudir a la Comisión Nacional para la Protección y Defensa de los Usuarios de Servicios Financieros en www.condusef.gob.mx , o a los teléfonos <a href="tel:5553400999">(55) 5340 0999</a>
+ y <a href="tel:8009998080">800 999 8080</a>.
                     </p>
                 </div>
 
