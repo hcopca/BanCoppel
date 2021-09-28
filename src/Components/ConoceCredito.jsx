7726c95ed81b07e45d7f2407623ccaaef5030057
yerim <yerim.morales@bahia360.mx>
2021-09-28 18:11:44 -0500

    change banner

--- src/Components/ConoceCredito.jsx
+++ src/Components/ConoceCredito.jsx
@@ -6,13 +6,86 @@ import PymesMobile from "../Assets/pymes_last_mobile.png";
 import Pymes from "../Assets/pymes.png";
 import BancoppelBtn from "./BancoppelBtn";
 
-const StyledBanner = styled.section``;
+const StyledBanner = styled.section`
+  .container {
+    padding-top: 25px;
+    padding-bottom: 25px;
+    position: relative;
+  }
+  .imgNormal {
+    display: none;
+  }
+
+  .img-holder {
+    img {
+      max-width: 100%;
+    }
+  }
+
+  .contenido_sobrepuesto {
+    position: absolute;
+    width: calc(100% - 30px);
+    height: calc(100% - 50px);
+    top: 25px;
+    display: flex;
+    align-items: flex-end;
+    justify-content: center;
+
+    a {
+      margin-bottom: 27px;
+      button {
+        width: 180px;
+        height: 60px;
+      }
+    }
+  }
+
+  @media (min-width: 576px) {
+  }
+
+  @media (min-width: 768px) {
+    .container {
+      padding-top: 50px;
+      padding-bottom: 50px;
+    }
+    .imgNormal {
+      display: block;
+    }
+
+    .imgResponsivo {
+      display: none;
+    }
+
+    .contenido_sobrepuesto {
+      width: calc(100% - 30px);
+      height: calc(100% - 100px);
+      top: 50px;
+      justify-content: flex-start;
+      a {
+        margin-bottom: 10px;
+        margin-left: 41px;
+      }
+    }
+  }
+
+  @media (min-width: 992px) {
+    .contenido_sobrepuesto {
+      a {
+        margin-bottom: 27px;
+        margin-left: 57px;
+      }
+    }
+  }
+
+  @media (min-width: 1200px) {
+  }
+`;
 
 export default class ConoceCredito extends Component {
   render() {
     return (
-      <Container>
-        <StyledBanner>
+      <StyledBanner>
+        <Container>
           <div className="img-holder">
             <img
               className="imgNormal"
@@ -27,15 +100,15 @@ export default class ConoceCredito extends Component {
               alt=""
               draggable="false"
             ></img>
-
-            <div className="contenido_sobrepuesto">
-              <a href="/contacto">
-                <BancoppelBtn amarillo>Solicita tu crédito</BancoppelBtn>
-              </a>
-            </div>
           </div>
-        </StyledBanner>
-      </Container>
+
+          <div className="contenido_sobrepuesto">
+            <a href="/contacto">
+              <BancoppelBtn amarillo>Solicita tu crédito</BancoppelBtn>
+            </a>
+          </div>
+        </Container>
+      </StyledBanner>
     );
   }
 }
